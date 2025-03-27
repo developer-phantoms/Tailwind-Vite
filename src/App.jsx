8fe427e7de02f14c390a62f33fe6006769fb659c
developer-phantoms <developer.phantoms@gmail.com>
2025-03-27 21:41:59 +0500

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import "./App.css"
+import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-4xl font-bold text-center'>Developer Ahmad</h1>
+      <h1 className="text-4xl font-bold text-center">Hello World</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
